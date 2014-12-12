7b264f33632b1fb11d9f24b12a92de55b426f492
Bartosz Nowak <sztosz@gmail.com>
2014-12-12 18:12:48 +0100

    Simplified

--- src/com/sztosz/commandLine1/Main.java
+++ src/com/sztosz/commandLine1/Main.java
@@ -1,18 +1,58 @@
 package com.sztosz.commandLine1;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        Controller controller = new Controller();
-        printDoubleValue("area", controller.getArea());
-        printDoubleValue("perimeter", controller.getPerimeter());
+    private Scanner input = new Scanner(System.in);
 
-    }
+    public static void main(String[] args) {
 
-    public static void printDoubleValue(String variableName, Double value){
-        System.out.println("Value of " + variableName + " is: " + value.toString());
+        Main me = new Main();
+        String input_string = null;
+        Double height;
+        Double baseWidth;
+        String classes[] = new String[] {"Square", "EquilateralTriangle", "RightTriangle", "Rectangle"};
+        Boolean dontBreakLoop = true;
+
+        while (dontBreakLoop) {
+            System.out.println("Please choose Geometric Figure:");
+            System.out.println("Square");
+            System.out.println("EquilateralTriangle");
+            System.out.println("RightTriangle");
+            System.out.println("Rectangle");
+            input_string = me.input.nextLine();
+            for (String cls : classes) {
+                if (input_string.equals(cls)) {
+                    dontBreakLoop = false;
+                }
+            }
+        }
+
+        System.out.println("Please enter base width :");
+        baseWidth = me.input.nextDouble();
+        System.out.println("Please enter height :");
+        height = me.input.nextDouble();
+
+        if (input_string.equals("Square")) {
+            Square figure = new Square(height, baseWidth);
+            System.out.println("Area :" + figure.getArea());
+            System.out.println("Perimeter :" + figure.getPerimeter());
+        } else if (input_string.equals("EquilateralTriangle")) {
+            EquilateralTriangle figure = new EquilateralTriangle(height, baseWidth);
+            System.out.println("Area :" + figure.getArea());
+            System.out.println("Perimeter :" + figure.getPerimeter());
+        } else if (input_string.equals("RightTriangle")) {
+            RightTriangle figure = new RightTriangle(height, baseWidth);
+            System.out.println("Area :" + figure.getArea());
+            System.out.println("Perimeter :" + figure.getPerimeter());
+        } else if (input_string.equals("Rectangle")) {
+            Rectangle figure = new Rectangle(height, baseWidth);
+            System.out.println("Area :" + figure.getArea());
+            System.out.println("Perimeter :" + figure.getPerimeter());
+        }
+
+
     }
 
 }
